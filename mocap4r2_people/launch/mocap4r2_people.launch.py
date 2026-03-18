126efd9300e4b599b8c987b1c0e66a0bf98d5bc5
Andrea Ostuni <andrea.ostuni@polito.it>
2026-03-18 14:05:41 +0000

    Add namespace declaration to launch file and update parameter format in YAML

--- mocap4r2_people/launch/mocap4r2_people.launch.py
+++ mocap4r2_people/launch/mocap4r2_people.launch.py
@@ -3,6 +3,8 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 import yaml
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
 
 package_name = "mocap4r2_people"
 
@@ -15,13 +17,23 @@ def dump_params(path, name):
         return [yaml.safe_load(file)[name]['ros__parameters']]
 
 def generate_launch_description():
-
+  namespace = LaunchConfiguration('namespace')
   mocap4r2_people = Node(
     package=package_name,
+    namespace=namespace,
     executable='people_program',
     name='mocap4r2_people',
+    remappings=[('/tf', 'tf'), ('/tf_static', 'tf_static')],
     output='screen',
     parameters=[os.path.join(get_package_share_directory(package_name), 'params', 'params.yaml')]
   )
 
-  return LaunchDescription([mocap4r2_people])
+  declare_namespace_cmd = DeclareLaunchArgument(
+    'namespace',
+    default_value='',
+    description='Namespace for the nodes'
+  )
+
+  return LaunchDescription([
+     declare_namespace_cmd,
+     mocap4r2_people])
